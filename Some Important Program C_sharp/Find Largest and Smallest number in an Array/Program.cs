86fd3e3629176f0d0c02955d60b342a830380878
Muhammad <faisalmahmud.eub@gmail.com>
2021-12-15 18:37:33 +0600

    Some program added

---
+++ Some Important Program C_sharp/Find Largest and Smallest number in an Array/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            float large, small;
+            int[] arr;
+            Console.WriteLine("Enter the size of the array: ");
+            string s = Console.ReadLine();
+            n = int.Parse(s);
+            arr = new int[n];
+            Console.WriteLine("Enter the array elements: ");
+            for (int i = 0; i < n; i++)
+            {
+                string s1 = Console.ReadLine();
+                arr[i] = int.Parse(s1);
+
+            }
+            Console.WriteLine("");
+            large = arr[0];
+            small = arr[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] > large)
+                {
+                    large = arr[i];
+                }
+                else if (arr[i] < small)
+                {
+                    small = arr[i];
+                }
+            }
+
+            Console.WriteLine($"largest number :{large}");
+            Console.WriteLine($"smallest number :{small}");
+
+        }
+    }
+}
+
+/*
+5
+45
+47
+675
+4
+45
+
+*/
---
+++ Some Important Program C_sharp/Get Length of the Array  ( 1D_and_2D )/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[6];
+            int len1 = arr1.Length;
+            Console.WriteLine($"Length of the 1D array: {len1}");
+
+            int[,] arr2 = new int[5, 10];
+            int len2 = arr2.Length;
+            Console.WriteLine($"length of the 2D arry: {len2}");
+
+        }
+    }
+}
