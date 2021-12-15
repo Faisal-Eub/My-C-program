6cce697951f271cd43eaee7233b809e5a22f4980
Muhammad <faisalmahmud.eub@gmail.com>
2021-12-15 19:09:14 +0600

    add

--- src/Jagged Array/Program.cs
+++ src/Jagged Array/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace JaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[2][];
+
+            arr[0] = new int[4] { 1, 3, 6, 10 };
+            arr[1] = new int[7] { 34, 45, 4, 54, 8, 7, 9 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Elements of row no {i+1} of jagged arry: ");
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write($"{arr[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
